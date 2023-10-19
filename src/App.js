1cd62bcd8ed1946fd2b7212c7e2e459f4be196ee
Jaivin Mungara <71208031+jaivin92@users.noreply.github.com>
2023-10-19 16:39:35 +0530

    Resolve all concern except map load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Router>
+            <div>
+              <Navbar/>
+            </div>
+            <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
+            </Routes>
+         </Router>
+
     </div>
   );
 }
